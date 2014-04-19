ee1e2db595cb159b7492f912637ec6ed3b914211
Thorsten Ball <mrnugget@gmail.com>
2014-04-19 08:23:46 +0200

    Add script to build releases

---
+++ dist.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Builds and packages the binaries for Darwin/amd64 and Linux/amd64
+
+version=$(cat fzz.go | grep "VERSION\s*=" | awk '{print $NF}' | sed 's/\"//g')
+arch=$(go env GOARCH)
+
+go test || (echo 'Tests failed. Stopping...' && exit 1)
+
+for os in linux darwin; do
+  target="watchgopher-$version-$os-$arch"
+  echo "Building $target ..."
+
+  mkdir -p dist/$target
+  go build -o dist/$target/watchgopher .
+  cp README.md dist/$target
+  cp LICENSE dist/$target
+
+  cd dist
+  tar czvf $target.tar.gz $target
+  cd -
+  rm -rf dist/$target
+done
