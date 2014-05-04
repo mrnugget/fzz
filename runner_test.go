9cbba18250a9337f115d4a3d22290f9d6adc0163
Thorsten Ball <mrnugget@gmail.com>
2014-05-04 20:02:55 +0200

    Remove or fix runner tests

--- runner_test.go
+++ runner_test.go
@@ -1,22 +1,8 @@
 package main
 
 import "bytes"
-import "fmt"
-import "io"
+import "sync"
 import "testing"
-
-type TestPrinter struct {
-	buffer io.Writer
-	reset  bool
-}
-
-func (t *TestPrinter) Print(line string) (n int, err error) {
-	return fmt.Fprint(t.buffer, line)
-}
-
-func (t *TestPrinter) Reset() {
-	t.reset = true
-}
 
 type TestPipe struct {
 	*bytes.Buffer
@@ -30,13 +16,16 @@ var testInput []byte = []byte{'f', 'o', 'o'}
 
 func TestStreamOutput(t *testing.T) {
 	pipe := &TestPipe{bytes.NewBufferString("foo\nbar\n")}
+	wg := &sync.WaitGroup{}
 	runner := &Runner{}
 
 	output := make([]string, 0)
-	ch := runner.streamOutput(pipe)
+	wg.Add(1)
+	ch := runner.streamOutput(pipe, wg)
 	for line := range ch {
 		output = append(output, line)
 	}
+	wg.Wait()
 
 	if len(output) != 2 {
 		t.Errorf("streamed output length wrong. expected: %d, actual: %d", 2, len(output))
@@ -53,13 +42,17 @@ func TestStreamOutput(t *testing.T) {
 
 func TestStreamOutputWithoutTrailingNewline(t *testing.T) {
 	pipe := &TestPipe{bytes.NewBufferString("foo\nbar")}
+	wg := &sync.WaitGroup{}
 	runner := &Runner{}
 
 	output := make([]string, 0)
-	ch := runner.streamOutput(pipe)
+	wg.Add(1)
+	ch := runner.streamOutput(pipe, wg)
+
 	for line := range ch {
 		output = append(output, line)
 	}
+	wg.Wait()
 
 	if len(output) != 2 {
 		t.Errorf("streamed output length wrong. expected: %d, actual: %d", 2, len(output))
@@ -71,82 +64,5 @@ func TestStreamOutputWithoutTrailingNewline(t *testing.T) {
 
 	if output[1] != "bar" {
 		t.Errorf("streamed output wrong. expected: %q, actual: %q", "bar", output[0])
-	}
-}
-
-
-func TestErrorPrinting(t *testing.T) {
-	buf := new(bytes.Buffer)
-
-	runner := &Runner{
-		template:    []string{"cat", "{{}}"},
-		placeholder: "{{}}",
-	}
-
-	runner.runWithInput([]byte{'d', 'o', 'e', 's', 'n', 'o', 't'})
-	output := buf.String()
-	if output != "cat: doesnot: No such file or directory\n" {
-		t.Errorf("unexpected output: %q", output)
-	}
-}
-
-// func TestOutputBuffering(t *testing.T) {
-// 	buf := new(bytes.Buffer)
-// 	testPrinter := &TestPrinter{
-// 		buffer: buf,
-// 		reset:  false,
-// 	}
-// 	outbuf := new(bytes.Buffer)
-// 
-// 	runner := &Runner{
-// 		template:    []string{"echo", "{{}}", "bar"},
-// 		placeholder: "{{}}",
-// 	}
-// 
-// 	n, err := runner.writeCmdStdout(outbuf)
-// 	if n != 0 {
-// 		t.Errorf("writeCmdStdout with empty buffer wrote wrong number of bytes: %d", n)
-// 	}
-// 	if err != nil {
-// 		t.Errorf("writeCmdStdout with empty buffer failed. error: %s", err)
-// 	}
-// 
-// 	runner.runWithInput(testInput)
-// 
-// 	printedOutput := buf.String()
-// 	if printedOutput != "foo bar\n" {
-// 		t.Errorf("unexpected printedOutput: %v", printedOutput)
-// 	}
-// 
-// 	_, err = runner.writeCmdStdout(outbuf)
-// 	if err != nil {
-// 		t.Errorf("writeCmdStdout failed. error: %s", err)
-// 	}
-// 
-// 	currentOutput := outbuf.String()
-// 	if currentOutput != printedOutput {
-// 		t.Errorf("unexpected output: %v", currentOutput)
-// 	}
-// }
-// 
-func TestStdin(t *testing.T) {
-	buf := new(bytes.Buffer)
-
-	stdinbuf := new(bytes.Buffer)
-	stdinbuf.WriteString("foo\nbar")
-
-	runner := &Runner{
-		template:    []string{"grep", "{{}}"},
-		placeholder: "{{}}",
-		stdinbuf:    *stdinbuf,
-	}
-
-	runner.runWithInput(testInput)
-	// Run it two times to see that the stdin is the same every time
-	runner.runWithInput(testInput)
-
-	printedOutput := buf.String()
-	if printedOutput != "foo\nfoo\n" {
-		t.Errorf("unexpected printedOutput: %q", printedOutput)
 	}
 }
