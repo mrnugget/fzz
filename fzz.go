b69f48cbf5d34fece79a87ad1c4d406d712981f7
Thorsten Ball <mrnugget@gmail.com>
2014-06-16 10:42:16 +0200

    Use separate files for Fzz and main()

--- fzz.go
+++ fzz.go
@@ -2,19 +2,14 @@ package main
 
 import (
 	"bytes"
-	"flag"
-	"fmt"
 	"io"
 	"log"
 	"os"
-	"os/signal"
 	"unicode"
 	"unicode/utf8"
 )
 
 const (
-	VERSION                   = "0.0.1"
-	defaultPlaceholder        = "{{}}"
 	keyBackspace              = 8
 	keyDelete                 = 127
 	keyEndOfTransmission      = 4
@@ -23,25 +18,6 @@ const (
 	keyEndOfTransmissionBlock = 23
 	keyEscape                 = 27
 )
-
-var placeholder string
-
-var usage = `fzz allows you to run a command interactively.
-
-Usage:
-
-	fzz command
-
-The command MUST include the placeholder '{{}}'.
-
-Arguments:
-
-	-v		Print version and exit
-`
-
-func printUsage() {
-	fmt.Printf(usage)
-}
 
 type Fzz struct {
 	tty           *TTY
@@ -129,68 +105,4 @@ func (fzz *Fzz) printRunnerOutput(out <-chan string, inputlen int) {
 		fzz.printer.Print(line)
 	}
 	fzz.tty.cursorAfterPrompt(inputlen)
-}
-
-func main() {
-	flVersion := flag.Bool("v", false, "Print fzz version and quit")
-	flag.Usage = printUsage
-	flag.Parse()
-
-	if *flVersion {
-		fmt.Printf("fzz %s\n", VERSION)
-		os.Exit(0)
-	}
-
-	if len(flag.Args()) < 2 {
-		fmt.Fprintf(os.Stderr, usage)
-		os.Exit(1)
-	}
-
-	if placeholder = os.Getenv("FZZ_PLACEHOLDER"); placeholder == "" {
-		placeholder = defaultPlaceholder
-	}
-
-	if !validPlaceholder(placeholder) {
-		fmt.Fprintln(os.Stderr, "Placeholder is not valid, needs even number of characters")
-		os.Exit(1)
-	}
-
-	input, args := extractInput(flag.Args(), placeholder)
-
-	if !containsPlaceholder(args, placeholder) {
-		fmt.Fprintln(os.Stderr, "No placeholder in arguments")
-		os.Exit(1)
-	}
-
-	tty, err := NewTTY()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer tty.resetState()
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		<-c
-		tty.resetState()
-		os.Exit(1)
-	}()
-	tty.setSttyState("cbreak", "-echo")
-
-	stdinbuf := bytes.Buffer{}
-	if isPipe(os.Stdin) {
-		io.Copy(&stdinbuf, os.Stdin)
-	}
-
-	printer := NewPrinter(tty, tty.cols, tty.rows-1) // prompt is one row
-
-	fzz := &Fzz{
-		printer:     printer,
-		tty:         tty,
-		stdinbuf:    &stdinbuf,
-		input:       []byte(input),
-		placeholder: placeholder,
-		args:        args,
-	}
-	fzz.Loop()
 }
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+)
+
+const (
+	VERSION                   = "0.0.1"
+	defaultPlaceholder        = "{{}}"
+)
+
+var placeholder string
+
+var usage = `fzz allows you to run a command interactively.
+
+Usage:
+
+	fzz command
+
+The command MUST include the placeholder '{{}}'.
+
+Arguments:
+
+	-v		Print version and exit
+`
+
+func printUsage() {
+	fmt.Printf(usage)
+}
+
+func main() {
+	flVersion := flag.Bool("v", false, "Print fzz version and quit")
+	flag.Usage = printUsage
+	flag.Parse()
+
+	if *flVersion {
+		fmt.Printf("fzz %s\n", VERSION)
+		os.Exit(0)
+	}
+
+	if len(flag.Args()) < 2 {
+		fmt.Fprintf(os.Stderr, usage)
+		os.Exit(1)
+	}
+
+	if placeholder = os.Getenv("FZZ_PLACEHOLDER"); placeholder == "" {
+		placeholder = defaultPlaceholder
+	}
+
+	if !validPlaceholder(placeholder) {
+		fmt.Fprintln(os.Stderr, "Placeholder is not valid, needs even number of characters")
+		os.Exit(1)
+	}
+
+	input, args := extractInput(flag.Args(), placeholder)
+
+	if !containsPlaceholder(args, placeholder) {
+		fmt.Fprintln(os.Stderr, "No placeholder in arguments")
+		os.Exit(1)
+	}
+
+	tty, err := NewTTY()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tty.resetState()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		tty.resetState()
+		os.Exit(1)
+	}()
+	tty.setSttyState("cbreak", "-echo")
+
+	stdinbuf := bytes.Buffer{}
+	if isPipe(os.Stdin) {
+		io.Copy(&stdinbuf, os.Stdin)
+	}
+
+	printer := NewPrinter(tty, tty.cols, tty.rows-1) // prompt is one row
+
+	fzz := &Fzz{
+		printer:     printer,
+		tty:         tty,
+		stdinbuf:    &stdinbuf,
+		input:       []byte(input),
+		placeholder: placeholder,
+		args:        args,
+	}
+	fzz.Loop()
+}
